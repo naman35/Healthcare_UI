475076d538bafc6608baf8fedb113d1fe7b6dd3d
Naman Agarwal <agarwalnaman35@gmail.com>
2022-03-21 17:49:19 +0530

    ReCommit(March-21)-Routing

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
+import { Routes ,Route,BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login from "./Components/login";
 import Signup from "./Components/signup";
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+        <Routes>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
+        </Routes>
+        </BrowserRouter>
        
-       <Signup />
+
 
     </div>
   );
