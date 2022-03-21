36fe5c51563ef893f0b3b71d07f5ea7d43df2044
Naman Agarwal <agarwalnaman35@gmail.com>
2022-03-21 18:45:43 +0530

    ReCommit(March-21)-states(login)

--- ui/src/Components/login.js
+++ ui/src/Components/login.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import {React,useState} from 'react'
 import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 const Login=()=>{
+    const [username, setusername] = useState("");
+    const [password, setPassword] = useState("");
 
+    function validateForm() {
+        return username.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
     const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
     const avatarStyle={backgroundColor:'#1bbd7e'}
     const btnstyle={margin:'8px 0'}
@@ -16,8 +25,8 @@ const Login=()=>{
                     <h2><i>Hii Buddy:-></i></h2>
                     <h2><i>Welcome Back</i></h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter username' fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <TextField  value={username} onChange={(e) => setusername(e.target.value)} label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField value={password}  onChange={(e) => setPassword(e.target.value)} label='Password' placeholder='Enter password' type='password' fullWidth required/>
                 <FormControlLabel
                     control={
                         <Checkbox
